857c4508879349da95513811c614463deaacd92d
ondrejkassak <ondrej.kassak@gmail.com>
2015-04-16 15:38:34 +0200

    more more more more more deployment stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,4 +52,13 @@ namespace :deploy do
     end
   end
 
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
 end
